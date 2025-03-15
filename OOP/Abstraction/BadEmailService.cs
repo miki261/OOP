76138ba256275a40dc3b28d72043c5d82f9abe33
Matej Marinkovic <matej261matej@gmail.com>
2025-03-15 12:03:21 +0100

    Resolved encapsulation

---
+++ OOP/Abstraction/BadEmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace OOP.abstraction;
+
+public class BadEmailService
+{
+    public void SendEmail() => Console.WriteLine("Sending Email");
+
+    public void Connect() => Console.WriteLine("Connecting");
+
+    public void Authenticate() => Console.WriteLine("Authenticating");
+
+    public void Disconnect() => Console.WriteLine("Disconnecting");
+}
---
+++ OOP/Encapsulation/BankAccountService.cs
@@ -0,0 +1,34 @@
+﻿namespace OOP.Encapsulation;
+
+public class BankAccountService
+{
+    private decimal balance;
+
+    public BankAccountService(decimal balance)
+    {
+        Deposit(balance);
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+
+    public void Deposit(decimal ammount)
+    {
+        if (ammount <= 0)
+        {
+            throw new ArgumentException("Deposit must be positive.");
+        }
+        this.balance += ammount;
+    }
+    public void Withdraw(decimal ammount)
+    {
+        if (ammount <= 0) 
+        {
+            throw new ArgumentException("Withdraw must be positive.");
+        }
+        if (ammount > balance) { 
+        }
+    }
+}
