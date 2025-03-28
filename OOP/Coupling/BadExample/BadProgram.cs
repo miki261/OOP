24cbf997cc7db77ac194c07ea5ab973c3470ab9f
Matej Marinkovic <matej261matej@gmail.com>
2025-03-28 09:47:13 +0100

    Lab 1 i 2

---
+++ OOP/Coupling/BadExample/BadProgram.cs
@@ -0,0 +1,22 @@
+﻿using OOP.Coupling.BadExample;
+
+namespace OOP.Coupling.BadExample;
+public class BadProgram
+{
+    public static void Main()
+    {
+        try
+        {
+            var orderService = new OrderService();
+            orderService.PlaceOrder();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Program completed.");
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP.Abstraction;
+using OOP.Aggregation;
+using OOP.Composition;
+using OOP.Encapsulation;
+
+
+// Encapsulation
+BadBankAccountServiceExample.Main();
+BankAccountServiceExample.Main();
+
+// Abstraction
+BadEmailServiceExample.Main();
+EmailServiceExample.Main();
+
+// Inheritance
+OOP.Inheritance.BadExample.BadProgram.Main();
+OOP.Inheritance.GoodExample.GoodProgram.Main();
+
+// Polymorphism
+OOP.Polymorphism.BadExample.BadProgram.Main();
+OOP.Polymorphism.GoodExample.GoodProgram.Main();
+OOP.Polymorphism.MethodOverloading.Main();
+OOP.Polymorphism.OperatorOverloading.Main();
+
+// Coupling
+OOP.Coupling.BadExample.BadProgram.Main();
+OOP.Coupling.GoodExample.GoodProgram.Main();
+
+// Composition
+CarExample.Main();
+
+// Aggregation
+Aggregation.Main();
